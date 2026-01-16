cfc146e846cafc8c4fbca84a5d840c9a52124d98
Anurag Patil <anurag151006@gmail.com>
2026-01-16 18:40:51 +0530

    Firebase Auth Added

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -1,18 +1,18 @@
 import { cookies } from "next/headers";
 import { createServerClient } from "@supabase/ssr";
 
-export const supabaseServer = () => {
-    const cookieStore = cookies();
+export const supabaseServer = async () => {
+  const cookieStore = await cookies(); 
 
-    return createServerClient(
-        process.env.NEXT_PUBLIC_SUPABASE_URL!,
-        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-        {
-            cookies: {
-                get(name: string) {
-                    return cookieStore.get(name)?.value;
-                },
-            },
-        }
-    );
+  return createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        get(name: string) {
+          return cookieStore.get(name)?.value; 
+        },
+      },
+    }
+  );
 };
